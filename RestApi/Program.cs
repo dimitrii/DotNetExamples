a5444edbc3cceeb61d7737c410a1553112342f83
Dimitrii Pokrovskii <dimitrii.pokrovskii@autodesk.com>
2018-12-20 10:39:10 -0800

    Added RestApi Example

---
+++ RestApi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace RestApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Request
+            var httpRequest = new HttpRequestMessage();
+            httpRequest.Method = HttpMethod.Post;
+            httpRequest.RequestUri = new Uri("https://developer.api.autodesk.com/authentication/v1/authenticate");
+            httpRequest.Content = new FormUrlEncodedContent(new Dictionary<string,string>() {
+                { "client_id", "" },
+                { "client_secret", "" },
+                { "grant_type", "client_credentials" },
+                { "scope", "data:read" },
+            });
+
+            // Send Request
+            using(var client = new HttpClient())
+            {
+                using (var reply = client.SendAsync(httpRequest).Result)
+                {
+                    using (HttpContent content = reply.Content)
+                    {
+                        var json = content.ReadAsStringAsync().Result;
+                        Console.WriteLine(json);
+                        var key = Console.ReadKey();
+                    }
+                }
+            }
+        }
+    }
+}
