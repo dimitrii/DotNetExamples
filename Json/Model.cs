40bfb024f99d65885946034c41f5c47e61fd48f0
Dimitrii Pokrovskii <dimitrii.pokrovskii@autodesk.com>
2018-12-20 13:26:27 -0800

    Added JSON example.

---
+++ Json/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Json
+{
+    [DataContract]
+    public class Model
+    {
+        [DataMember]
+        public string Prop1 {get;set;}
+        [DataMember]
+        public string Prop2 {get;set;}
+    }
+}
---
+++ Json/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Object
+            var model = new Model()
+            {
+                Prop1 = "proeprty 1",
+                Prop2 = "property 2"                
+            };
+
+            // Turn Object to JSON string
+            var modelJsonString = String.Empty;
+            using (var ms = new MemoryStream())
+            {
+                var serializer = new DataContractJsonSerializer(typeof(Model));
+                serializer.WriteObject(ms, model);
+                ms.Position = 0;
+                using (var sr = new StreamReader(ms))
+                {
+                    modelJsonString = sr.ReadToEnd();
+                }
+            }
+            Console.WriteLine(modelJsonString);
+
+            // Turn Json string to object
+            Model modelFromJsonString;
+            using(var ms = new MemoryStream(Encoding.UTF8.GetBytes(modelJsonString)))
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Model));
+                try
+                {
+                    modelFromJsonString = (Model)serializer.ReadObject(ms);
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+            Console.WriteLine($"Prop1: {modelFromJsonString.Prop1}");
+            Console.WriteLine($"Prop2: {modelFromJsonString.Prop2}");
+        }
+    }
+}
