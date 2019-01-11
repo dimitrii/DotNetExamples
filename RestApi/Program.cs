c019ac3b65c0ee4432066fc47ba39c19be9a2ca4
Dimitrii Pokrovskii <dimitrii.pokrovskii@autodesk.com>
2019-01-10 18:11:50 -0800

    Added Console In

--- RestApi/Program.cs
+++ RestApi/Program.cs
@@ -8,13 +8,19 @@ namespace RestApi
     {
         static void Main(string[] args)
         {
+            Console.Write("Forge Client Id: ");
+            var clientId = Console.ReadLine();
+
+            Console.Write("Forge Client Secret: ");
+            var clientSecret = Console.ReadLine();
+
             // Create Request
             var httpRequest = new HttpRequestMessage();
             httpRequest.Method = HttpMethod.Post;
             httpRequest.RequestUri = new Uri("https://developer.api.autodesk.com/authentication/v1/authenticate");
             httpRequest.Content = new FormUrlEncodedContent(new Dictionary<string,string>() {
-                { "client_id", "" },
-                { "client_secret", "" },
+                { "client_id", clientId },
+                { "client_secret", clientSecret },
                 { "grant_type", "client_credentials" },
                 { "scope", "data:read" },
             });
