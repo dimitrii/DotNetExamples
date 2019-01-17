125f89c375e8dffcfe883c967e161835ff3778c7
Dimitrii Pokrovskii <dimitrii.pokrovskii@autodesk.com>
2019-01-17 12:08:17 -0800

    Added a console app that creates files of a given size.

---
+++ MakeFileOfSizeX/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace MakeFileOfSizeX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many megabytes? (1000 max): ");
+            var sizeInMbString = Console.ReadLine();
+
+            int sizeInMb = Convert.ToInt32(sizeInMbString);
+
+            if (sizeInMb >= 1000 || sizeInMb < 0)
+            {
+                return;
+            }
+            var fileName = Guid.NewGuid().ToString() + ".dat";
+            byte[] data = new byte[sizeInMb * 1024 * 1024];
+            Random rng = new Random();
+            rng.NextBytes(data);
+            File.WriteAllBytes(fileName, data);
+
+            Console.WriteLine("Saved: " + fileName);
+        }
+    }
+}
